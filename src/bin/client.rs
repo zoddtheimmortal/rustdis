d77eafd53c65b19f8600d0980bd937fb18c5efb1
Alok Desai <alokdesai2004@gmail.com>
2024-08-28 15:47:24 +0530

    chore: add response printer to client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,11 +1,31 @@
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use bytes::BytesMut;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
 
     // command format -> <cmd name> <key> <value>
-    stream.write_all(b"set key value").await?;
+    stream.write_all(b"set foo bar").await?;
+
+    let mut buf = BytesMut::with_capacity(1024);
+    let _len = stream.read_buf(&mut buf).await?;
+
+    match std::str::from_utf8(&mut buf) {
+        Ok(resp) => {
+            if resp == "r Ok" {
+                println!("key updated");
+            } else if resp == "Ok" {
+                println!("key set")
+            }
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+        }
+    }
 
     Ok(())
 }
