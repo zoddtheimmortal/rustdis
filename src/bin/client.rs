deb0d118a2e4f7582e0f791f6115bcbf9b454f2e
Alok Desai <alokdesai2004@gmail.com>
2024-08-28 15:06:44 +0530

    chore: update client command format

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,7 +4,8 @@ use tokio::{io::AsyncWriteExt, net::TcpStream};
 pub async fn main() -> Result<(), std::io::Error> {
     let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
 
-    stream.write_all(b"Hello Server!").await?;
+    // command format -> <cmd name> <key> <value>
+    stream.write_all(b"set key value").await?;
 
     Ok(())
 }
