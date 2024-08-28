9a18dd8f9b87e558e4a2c7f4bdd779bf10545196
Alok Desai <alokdesai2004@gmail.com>
2024-08-28 16:07:47 +0530

    feat: add clap to accept cli cmds

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,49 +1,80 @@
 use bytes::BytesMut;
+use clap::{Parser, Subcommand};
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
 
+#[derive(Parser, Debug)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: String },
+}
+
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
+    let args = Cli::parse();
+    let mut stream = TcpStream::connect("127.0.0.1:8081").await.unwrap();
 
-    // command format -> <cmd name> <key> <value>
-    stream.write_all(b"set foo bar").await?;
+    match args.command {
+        Command::Set { key, value } => {
+            // set format -> <cmd name> <key> <value>
 
-    let mut buf = BytesMut::with_capacity(1024);
-    let _len = stream.read_buf(&mut buf).await?;
+            stream.write_all(b"set").await?;
+            stream.write_all(b" ").await?;
 
-    match std::str::from_utf8(&mut buf) {
-        Ok(resp) => {
-            if resp == "r Ok" {
-                println!("key updated");
-            } else if resp == "Ok" {
-                println!("key set")
+            stream.write_all(&key.as_bytes()).await?;
+            stream.write_all(b" ").await?;
+
+            stream.write_all(&value.as_bytes()).await?;
+            let mut buf = BytesMut::with_capacity(1024);
+            let _len = stream.read_buf(&mut buf).await?;
+
+            match std::str::from_utf8(&mut buf) {
+                Ok(resp) => {
+                    if resp == "r Ok" {
+                        println!("key updated");
+                    } else if resp == "Ok" {
+                        println!("key set")
+                    }
+                }
+                Err(err) => {
+                    println!("Error: {}", err);
+                }
             }
         }
-        Err(err) => {
-            println!("Error: {}", err);
-        }
-    }
 
-    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
-    stream.write_all(b"get foo").await?;
+        Command::Get { key } => {
+            //get format -> <cmd name> <key>
 
-    let mut buf = BytesMut::with_capacity(1024);
-    let _len = stream.read_buf(&mut buf).await?;
-    println!("buffer: {:?}", buf);
+            stream.write_all(b"get").await?;
+            stream.write_all(b" ").await?;
 
-    match std::str::from_utf8(&mut buf) {
-        Ok(resp) => {
-            if resp == "" {
-                println!("key not found");
-            } else {
-                println!("value: {}", resp);
+            stream.write_all(&key.as_bytes()).await?;
+
+            let mut buf = BytesMut::with_capacity(1024);
+            let _len = stream.read_buf(&mut buf).await?;
+            println!("buffer: {:?}", buf);
+
+            match std::str::from_utf8(&mut buf) {
+                Ok(resp) => {
+                    if resp == "" {
+                        println!("key not found");
+                    } else {
+                        println!("value: {}", resp);
+                    }
+                }
+                Err(_err) => {
+                    println!("Error: {}", _err);
+                }
             }
-        }
-        Err(_err) => {
-            println!("Error: {}", _err);
         }
     }
 
